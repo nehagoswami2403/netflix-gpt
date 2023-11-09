56ca96e34663640ed810931bed245efa4e3a930d
nehagoswami2403 <nehagoswami2403@gmail.com>
2023-11-09 16:31:55 +0530

    header of netflix-gpt done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
 
 const Body = () => {
+
+    const appRouter = createBrowserRouter(
+        [
+            {
+                path: "/",
+                element: <Login />,
+            },
+
+            {
+                path: "/browse",
+                element: <Browse />,
+
+            }
+        ]
+    );
     return (
         <div>
-            <Login />
-            <Browse />
+            <RouterProvider router={appRouter} />
         </div>
     )
 }
